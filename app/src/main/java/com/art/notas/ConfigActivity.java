a1d80eb49876f2c6486248d994e7477dcbdde25f
artpin107 <artpin.107@gmail.com>
2018-04-13 18:15:23 -0300

    App

--- app/src/main/java/com/art/notas/ConfigActivity.java
+++ app/src/main/java/com/art/notas/ConfigActivity.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,12 +15,23 @@ import android.widget.EditText;
 
 public class ConfigActivity extends Activity {
 
+    Button buttonG;
+
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
 
+        buttonG = findViewById(R.id.buttonGit);
+        buttonG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/artpin107/Notas2");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
 
 
     }

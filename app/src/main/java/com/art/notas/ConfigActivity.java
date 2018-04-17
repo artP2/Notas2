8fc21f73c6951a98865ea5df5ffaec0f26746c78
artpin107 <artpin.107@gmail.com>
2018-04-17 17:56:12 -0300

    App

--- app/src/main/java/com/art/notas/ConfigActivity.java
+++ app/src/main/java/com/art/notas/ConfigActivity.java
@@ -1,49 +1,49 @@
 package com.art.notas;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ConfigActivity extends Activity {
 
     Button buttonG , buttonR;
+    ImageView imageViewR;
 
-    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config);
 
-        buttonG =   findViewById(R.id.buttonSource);
-        buttonR =   findViewById(R.id.buttonRequest);
+        buttonG     =   findViewById(R.id.buttonSource);
+        buttonR     =   findViewById(R.id.buttonRequest);
+        imageViewR  =   findViewById(R.id.imageReturn);
 
         buttonG.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse("https://github.com/artpin107/Notas2");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+                Uri uri =   Uri.parse( "https://github.com/artpin107/Notas2" );
+                Intent intent   =   new Intent( Intent.ACTION_VIEW , uri );
+                startActivity( intent );
             }
         });
-
         buttonR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = Uri.parse("https://t.me/artP2");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+                Uri uri =   Uri.parse( "https://t.me/artP2" );
+                Intent intent   =   new Intent( Intent.ACTION_VIEW , uri );
+                startActivity( intent );
             }
         });
-
+        imageViewR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConfigActivity.this.finish();
+            }
+        });
     }
 
 }
--- app/src/main/java/com/art/notas/MainActivity.java
+++ app/src/main/java/com/art/notas/MainActivity.java
@@ -8,7 +8,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
@@ -19,7 +18,6 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -30,13 +28,13 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
 
 
-    private Button PAdd , PCancel , openConfig , imageAdd;
-    private EditText PText;
-    private ListView listNotas;
-    private SQLiteDatabase bancoDados;
-    private ArrayAdapter<String> arrayAdapter;
-    private ArrayList<String> notas;
-    private ArrayList<Integer> ids;
+    private Button      PAdd , PCancel , openConfig , imageAdd;
+    private EditText    PText;
+    private ListView    listNotas;
+    private SQLiteDatabase          bancoDados;
+    private ArrayAdapter<String>    arrayAdapter;
+    private ArrayList<String>       notas;
+    private ArrayList<Integer>      ids;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -45,21 +43,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
 //**************************************************************************************************
-        listNotas = findViewById(R.id.listViewNotes);
-        openConfig = findViewById(R.id.buttonConfig);
-        imageAdd = findViewById(R.id.buttonAdd);
+        listNotas   =   findViewById(R.id.listViewNotes);
+        openConfig  =   findViewById(R.id.buttonConfig);
+        imageAdd    =   findViewById(R.id.buttonAdd);
 
 //**************************************************************************************************
 // Criar arquivo para guardar dados
-        bancoDados = openOrCreateDatabase("appnotas",MODE_PRIVATE,null);
+        bancoDados  =   openOrCreateDatabase("appnotas",MODE_PRIVATE,null);
         bancoDados.execSQL("CREATE TABLE IF NOT EXISTS notas(id INTEGER PRIMARY KEY AUTOINCREMENT, nota VARCHAR ) ");
 
 //**************************************************************************************************
 // PopUp
-        AlertDialog.Builder alertDialogBuider  = new AlertDialog.Builder(MainActivity.this);
+        AlertDialog.Builder alertDialogBuider   =   new AlertDialog.Builder(MainActivity.this);
         alertDialogBuider.setView(R.layout.custom_popup);
 
-        AlertDialog alertDialog = alertDialogBuider.create();
+        AlertDialog alertDialog =   alertDialogBuider.create();
         alertDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
 
         imageAdd.setOnClickListener(new View.OnClickListener() {
@@ -67,9 +65,9 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 alertDialog.show();
 
-                PAdd = alertDialog.findViewById(R.id.buttonPAdd);
-                PCancel = alertDialog.findViewById(R.id.buttonPCancel);
-                PText = alertDialog.findViewById(R.id.editTextP);
+                PAdd    =   alertDialog.findViewById(R.id.buttonPAdd);
+                PCancel =   alertDialog.findViewById(R.id.buttonPCancel);
+                PText   =   alertDialog.findViewById(R.id.editTextP);
 
                 alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
@@ -85,7 +83,7 @@ public class MainActivity extends Activity {
                 PAdd.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        String PTextoD = PText.getText().toString();
+                        String PTextoD  =   PText.getText().toString();
                         if (PTextoD.equals("")){
                             Toast.makeText(MainActivity.this, R.string.nota_nula, Toast.LENGTH_SHORT).show();
                         }else{
@@ -148,24 +146,24 @@ public class MainActivity extends Activity {
             try{
 
                 //Recuperar as tarefas
-                Cursor cursor = bancoDados.rawQuery("SELECT * FROM notas ORDER BY id DESC", null);
+                Cursor cursor   =   bancoDados.rawQuery("SELECT * FROM notas ORDER BY id DESC", null);
 
                 //recuperar os ids das colunas
-                int indiceColunaId = cursor.getColumnIndex("id");
-                int indiceColunaNota = cursor.getColumnIndex("nota");
+                int indiceColunaId      =   cursor.getColumnIndex("id");
+                int indiceColunaNota    =   cursor.getColumnIndex("nota");
 
                 //Criar adaptador
-                notas = new ArrayList<String>();
-                ids = new ArrayList<Integer>();
-                arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                notas           =   new ArrayList<String>();
+                ids             =   new ArrayList<Integer>();
+                arrayAdapter    =   new ArrayAdapter<String>(getApplicationContext(),
                     android.R.layout.simple_list_item_2,
                     android.R.id.text2,
                     notas){
                 @Override
                 public View getView(int position, View convertView, ViewGroup parent) {
 
-                    View view = super.getView(position, convertView, parent);
-                    TextView text = (TextView) view.findViewById(android.R.id.text2);
+                    View view       =   super.getView(position, convertView, parent);
+                    TextView text   =   view.findViewById(android.R.id.text2);
                     text.setTextColor(Color.WHITE);
                     return view;
 
@@ -178,8 +176,8 @@ public class MainActivity extends Activity {
             while ( cursor != null ){
 
                 Log.i("Resultado - ", "Id nota: " + cursor.getString( indiceColunaId ) + " nota: " + cursor.getString( indiceColunaNota ) );
-                notas.add(cursor.getString(indiceColunaNota));
-                ids.add( Integer.parseInt(cursor.getString(indiceColunaId)) );
+                notas.add( cursor.getString( indiceColunaNota ) );
+                ids.add( Integer.parseInt( cursor.getString( indiceColunaId ) ) );
 
                 cursor.moveToNext();
             }
@@ -200,10 +198,10 @@ public class MainActivity extends Activity {
     }
     private void svNota(String texto){
         // Solve de ' problem
-        String textoR =  texto.replace("'","''");
+        String textoR   =   texto.replace("'","''");
         try {
-                bancoDados.execSQL("INSERT INTO notas (nota) VALUES('" + textoR + "') ");
-                recuperarNotas();
+            bancoDados.execSQL("INSERT INTO notas (nota) VALUES('" + textoR + "') ");
+            recuperarNotas();
 
         }catch (Exception e){
             e.printStackTrace();
